4a7ef59f1cf0069d11987cd254d28cb8c01b3a73
Miha Mlinaric <mlinmiha@gmail.com>
2022-12-06 10:52:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 const app = express(); //celotna aplikacija v app
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist/chess-ang'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/chess-ang/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/chess-ang/index.html')));
 
 app.listen(PORT);
 
