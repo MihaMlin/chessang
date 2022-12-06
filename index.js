f3d0bd0c4c441b89f386768700d12db644a181be
Miha Mlinaric <mlinmiha@gmail.com>
2022-12-06 10:49:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 
 const app = express(); //celotna aplikacija v app
-const PORT = rocess.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist/chess-ang'));
